c62640a00d419a95c7dd9101a2c7d83065046c38
JamesxGamesYT <jamesanxinlian@gmail.com>
2020-01-07 16:10:40 -0500

    Added close function to dashboard

--- src/gui/Dashboard.java
+++ src/gui/Dashboard.java
@@ -1,5 +1,21 @@
 package gui;
+import gui.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class Dashboard {
 
+public class Dashboard  {
+	Dashboard() {
+		JFrame window = new JFrame();
+		window.setVisible(true);
+		
+	}
+	
+	public void close() {
+		System.exit(0);
+	}
+	
+	public static void main(String[] args) {
+		Dashboard dashboard = new Dashboard();
+	}
 }
