886fc3cfbec1541a7dc20ba5c9b209f596256799
JamesxGamesYT <jamesanxinlian@gmail.com>
2020-01-07 21:51:56 -0500

    started basic layout

--- src/gui/Dashboard.java
+++ src/gui/Dashboard.java
@@ -1,14 +1,34 @@
 package gui;
 import gui.*;
 import javax.swing.*;
+import java.awt.GraphicsConfiguration;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.*;
 
 
 public class Dashboard  {
 	Dashboard() {
+		GraphicsConfiguration gc;
 		JFrame window = new JFrame();
-		window.setVisible(true);
+		JLabel title = new JLabel();
+		title.setText("m " + "neybook");
+		title.setBounds(400,200,100,100);
+		JMenuBar sidebar = new JMenuBar();
+		sidebar.setSize(256, 1080);
+		sidebar.setBackground(Color.BLACK);
 		
+		
+		window.add(sidebar);
+		window.add(title);
+		
+		window.setSize(1920,1080);
+		window.setBackground(Color.WHITE);
+		window.setTitle("moneybook");
+		window.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+		window.setLayout(null);
+		window.setVisible(true);
+		window.setDefaultCloseOperation(1);
 	}
 	
 	public void close() {
