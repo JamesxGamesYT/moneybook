22e664fcd305c789b5c5a27f2b945351493b3a57
JamesxGamesYT <jamesanxinlian@gmail.com>
2020-03-14 09:58:00 -0400

    -

--- src/gui/Dashboard.java
+++ src/gui/Dashboard.java
@@ -3,6 +3,7 @@ import gui.*;
 import javax.swing.*;
 import java.awt.GraphicsEnvironment;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.event.*;
@@ -14,41 +15,41 @@ import java.io.InputStream;
 
 
 public class Dashboard  {
-	private String customfont;
 
 	Dashboard() {
-		customfont = "yes";
-		JFrame window = new JFrame();
+		JFrame screen = new JFrame();
+		JPanel window = new JPanel();
 		JLabel title = new JLabel();
-		try {
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			File xkcd = new File("./bin/gui/xkcd-script.ttf");
-			System.out.println(xkcd.getCanonicalPath());
-			Font xkcdscript = Font.createFont(0, xkcd);
-			ge.registerFont(xkcdscript);
-			title.setFont(xkcdscript);
-		} catch (IOException | FontFormatException e) {
-			System.out.println(e);
-			customfont = "no";
-		}
+
 		title.setText("m " + "neybook");
 		title.setBounds(400,200,100,100);
-		
+		title.setVisible(true);
+		window.add(title);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		File xkcd = new File("./bin/gui/xkcd-script.ttf");
+		Font xkcdscript;
+		try {
+			xkcdscript = Font.createFont(0, xkcd);
+			ge.registerFont(xkcdscript);
+			title.setFont(xkcdscript);
+		} catch (FontFormatException | IOException e) {
+			e.printStackTrace();
+		}
 		JMenuBar sidebar = new JMenuBar();
+		
 		sidebar.setBounds(0, 50, 250, 800);
 		sidebar.setBackground(Color.BLACK);
 		
-		
+		window.setLayout(new FlowLayout());
 		window.add(sidebar);
 		window.add(title);
 		
-		window.setSize(1920,1080);
-		window.setBackground(Color.WHITE);
-		window.setTitle("moneybook");
-		window.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-		window.setLayout(null);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(1);
+		screen.add(window);
+		screen.setBackground(Color.WHITE);
+		screen.setTitle("moneybook");
+		screen.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+		screen.setDefaultCloseOperation(1);
+		screen.setVisible(true);
 	}
 	
 	public void close() {
@@ -57,6 +58,5 @@ public class Dashboard  {
 	
 	public static void main(String[] args) {
 		Dashboard dashboard = new Dashboard();
-		System.out.println(dashboard.customfont);
 	}
 }
